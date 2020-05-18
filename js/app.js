7d2d2a1066a38a7f8ca807a270f8ca469cfa3130
Sean Murray <sean.j.murray1@gmail.com>
2020-05-18 14:36:54 -0700

    fish now turn based on direction

--- js/app.js
+++ js/app.js
@@ -9,29 +9,39 @@ function pauseAudio(){
   audioPlayer.pause();
 }
 
-playAudio();
-pauseAudio();
+var dpi = window.devicePixelRatio;
 
 var canvas = document.getElementById('myCanvas');
 var ctx = canvas.getContext('2d');
+var fish = document.getElementById('fish');
+var fishFlip = document.getElementById('fishFlip');
+ctx.imageSmoothingEnabled = false;
+ctx.imageSmoothingQuality = 'high';
+var fH = fish.height;
+var fW = fish.width;
 var x = canvas.width/2;
 var y = canvas.height-30;
 var dx = Math.random();
 var dy = -(Math.random() / 2);
-var ballRadius = 10;
+var ballRadius = 20;
 var count = 0;
 var maybe = ()=>Math.floor(Math.random()*2);
-
-function drawBall() {
-  ctx.beginPath();
-  ctx.rect(x, y, ballRadius,ballRadius);
-  ctx.fillStyle = '#FF6600';
-  ctx.fill();
-  ctx.closePath();
+function fix_dpi() {
+  var style_height = +getComputedStyle(canvas).getPropertyValue("height").slice(0, -2);
+  var style_width = +getComputedStyle(canvas).getPropertyValue("width").slice(0, -2);
+  canvas.setAttribute('height', style_height * dpi);
+  canvas.setAttribute('width', style_width * dpi);
 }
+fix_dpi();
+
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  drawBall();
+  // drawBall();
+  if(Math.sign(dx)===1){
+    ctx.drawImage(fishFlip,0,0,fW,fH,x,y,fW/2,fH/2);
+  }else{
+    ctx.drawImage(fish,0,0,fW,fH,x,y,fW/2,fH/2);
+  }
   x += dx;
   y += dy;
   if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
@@ -53,8 +63,6 @@ function draw() {
   }
 }
 setInterval(draw,20);
-
-
 
 
 
