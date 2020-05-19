bd7cd43026f2fcd9561725aeb04c18720efcb9ee
Sean Murray <sean.j.murray1@gmail.com>
2020-05-18 18:20:54 -0700

    Add constructor

--- js/app.js
+++ js/app.js
@@ -8,62 +8,67 @@ function playAudio(){
 function pauseAudio(){
   audioPlayer.pause();
 }
-
-
+var fishies = [];
 var canvas = document.getElementById('myCanvas');
 canvas.width = canvas.scrollWidth;
 canvas.height = canvas.scrollHeight;
 var ctx = canvas.getContext('2d');
 var fish = new Image();
-fish.src = './img/fish.png';
+fish.src = 'img/fish.png';
 var fishFlip = new Image();
-fishFlip.src = './img/fishflip.png';
+fishFlip.src = 'img/fishflip.png';
 ctx.imageSmoothingEnabled = false;
 ctx.imageSmoothingQuality = 'high';
 var fH = fish.height;
 var fW = fish.width;
 var x = canvas.width/2;
 var y = canvas.height-100;
-var dx = Math.random();
-var dy = -(Math.random() / 2);
 var ballRadius = 10;
-var count = 0;
-var maybe = ()=>Math.floor(Math.random()*2);
 
-function draw() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  if(!fish.complete || !fishFlip.complete){
-    setTimeout(function(){
-      draw();
-    },50);
-  }
-  if(Math.sign(dx)===1){
-    ctx.drawImage(fishFlip,0,0,fW,fH,x,y,fW/2,fH/2);
-  }else{
-    ctx.drawImage(fish,0,0,fW,fH,x,y,fW/2,fH/2);
-  }
-  console.log('here');
-  x += dx;
-  y += dy;
-  if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
-    dx = -dx;
-  }
-  if(y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
-    dy = -dy;
-  }
-  count++;
-  if(count>10000){
-    count = 0;
-    if(maybe()>0){
-      dx = Math.random();
-      dy = Math.random() / 2;
-    } else{
-      dx = -(Math.random());
-      dy = -(Math.random() / 2);
+var NewFish = function(img,flipImg){
+  this.dx= Math.random();
+  this.dy= -(Math.random());
+  this.img=img;
+  this.flipImg=flipImg;
+  this.count=0;
+  fishies.push(this);
+};
+NewFish.prototype.maybe = ()=>Math.floor(Math.random()*2);
+
+NewFish.prototype.draw = function(){
+  setInterval(function(){
+    debugger;
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    if(Math.sign(this.dx)===1){
+      ctx.drawImage(this.flipImg,0,0,fW,fH,x,y,fW/2,fH/2);
+    }else{
+      ctx.drawImage(this.img,0,0,fW,fH,x,y,fW/2,fH/2);
     }
-  }
-}
-setInterval(draw,20);
+    console.log(this.count);
+    x += this.dx;
+    y += this.dy;
+    if(x + this.dx > canvas.width-ballRadius || x + this.dx < ballRadius) {
+      this.dx = -this.dx;
+    }
+    if(y + this.dy > canvas.height-ballRadius || y + this.dy < ballRadius) {
+      this.dy = -this.dy;
+    }
+    this.count++;
+    if(this.count>10000){
+      this.count = 0;
+      if(this.maybe()>0){
+        this.dx = Math.random();
+        this.dy = Math.random() / 2;
+      } else{
+        this.dx = -(Math.random());
+        this.dy = -(Math.random() / 2);
+      }
+    }
+  },20);
+};
+var test = new NewFish(fish,fishFlip);
+
+
 
 
 
