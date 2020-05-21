829ab4259c081fc57c0ae6dd284aaf2918bbee3c
SBALDOCK <stephenbaldock78@gmail.com>
2020-05-21 08:08:31 -0700

    correcting time profile js file

--- js/profile.js
+++ js/profile.js
@@ -6,15 +6,15 @@ function colorProfile(){
     document.body.style.backgroundColor = '#00caff';
     document.body.style.backgroundImage = 'radial-gradient(circle at top right, rgba(0,0,0,0) 50%, rgba(0,0,0,0.8) 100%';
   }
-  if (11 <= currentTime && currentTime < 16){
+  else if (11 <= currentTime && currentTime < 16){
     document.body.style.background = '#00b4ff';
     document.body.style.backgroundImage = 'radial-gradient(circle at top, rgba(0,0,0,0) 50%, rgba(0,0,0,0.8) 100%';
   }
-  if (16 <= currentTime && currentTime < 21){
+  else if (16 <= currentTime && currentTime < 21){
     document.body.style.backgroundColor = '#0096d5';
     document.body.style.backgroundImage = 'radial-gradient(circle at top left, rgba(0,0,0,0) 50%, rgba(0,0,0,0.8) 100%';
   }
-  if (21 <= currentTime && currentTime < 6){
+  else {
     document.body.style.backgroundColor = '#24222f';
     document.body.style.backgroundImage = 'radial-gradient(circle at top, rgba(0,0,0,0) 50%, rgba(0,0,0,0.4) 100%';
   }
